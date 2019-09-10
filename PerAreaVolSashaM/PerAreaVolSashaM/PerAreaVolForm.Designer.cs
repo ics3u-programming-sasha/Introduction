ef98bb0e64b3ae5bbacc7f4f67e5e478139e6572
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-09-10 13:09:53 -0400

    Made changes

---
+++ PerAreaVolSashaM/PerAreaVolSashaM/PerAreaVolForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PerAreaVolSashaM
+{
+    partial class frmPerAreaVol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQPerimeter = new System.Windows.Forms.Button();
+            this.lblAPerimeter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnQPerimeter
+            // 
+            this.btnQPerimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnQPerimeter.Location = new System.Drawing.Point(106, 68);
+            this.btnQPerimeter.Name = "btnQPerimeter";
+            this.btnQPerimeter.Size = new System.Drawing.Size(252, 102);
+            this.btnQPerimeter.TabIndex = 0;
+            this.btnQPerimeter.Text = "Calculate the perimeter of a rectangle with a length of 8cm and a width of 5cm.";
+            this.btnQPerimeter.UseVisualStyleBackColor = true;
+            // 
+            // lblAPerimeter
+            // 
+            this.lblAPerimeter.AutoSize = true;
+            this.lblAPerimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblAPerimeter.Location = new System.Drawing.Point(515, 107);
+            this.lblAPerimeter.Name = "lblAPerimeter";
+            this.lblAPerimeter.Size = new System.Drawing.Size(45, 25);
+            this.lblAPerimeter.TabIndex = 1;
+            this.lblAPerimeter.Text = "???";
+            this.lblAPerimeter.Click += new System.EventHandler(this.LblAPerimeter_Click);
+            // 
+            // frmPerAreaVol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAPerimeter);
+            this.Controls.Add(this.btnQPerimeter);
+            this.Name = "frmPerAreaVol";
+            this.Text = "Perimeter, Area, Volume by Sasha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQPerimeter;
+        private System.Windows.Forms.Label lblAPerimeter;
+    }
+}
+
---
+++ PerAreaVolSashaM/PerAreaVolSashaM/PerAreaVolForm.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by: Sasha Malko
+ * Created on: 10-Sep-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #7 - Perimeter, Area, Volume
+ * This program calculates the perimeter of a rectangle, the area of a circle, and the volume of a sphere. It displays the answers in labels when the respective button is clicked. 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerAreaVolSashaM
+{
+    public partial class frmPerAreaVol : Form
+    {
+        public frmPerAreaVol()
+        {
+            InitializeComponent();
+        }
+
+        private void LblAPerimeter_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
