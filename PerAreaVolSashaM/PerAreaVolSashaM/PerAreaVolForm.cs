50cff5a9d63fcf0ca304d0c20ca62adef573f8c3
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-09-13 11:51:09 -0400

    Made changes

--- PerAreaVolSashaM/PerAreaVolSashaM/PerAreaVolForm.cs
+++ PerAreaVolSashaM/PerAreaVolSashaM/PerAreaVolForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Sasha Malko
+ * Created on: 10-Sep-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #7 - Perimeter, Area & Volume
+ * This program calculates the perimeter of a rectangle, the area of a circle, and the volume of a sphere. 
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
